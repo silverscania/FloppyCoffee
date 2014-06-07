d5c7c502c25bb43b93e50cf149fcdc35a71b563f
James Clark <silverscania@gmail.com>
2014-06-07 17:47:50 +0100

    disk monitor works, adding command line options

--- main.c
+++ main.c
@@ -15,6 +15,7 @@
 #include <string.h>
 #include <dbus/dbus.h>       // for dbus_*   
 #include <dbus/dbus-glib.h>  // for dbus_g_*
+#include <bcm2835.h>
 
 // File system is:
 // {DiskType - Time, Hour, Minute}
@@ -30,7 +31,7 @@
 typedef enum {TIME, VARIETY} DiskType;
 typedef enum {ESPRESSO, AMERICANO} Variety;
 
-//build gcc main.c -std=c99 -I/usr/include/dbus-1.0 -I/usr/lib/arm-linux-gnueabihf/dbus-1.0/include/ -I/usr/include/glib-2.0 -I/usr/lib/arm-linux-gnueabihf/glib-2.0/include/ -ldbus-1 -ldbus-glib-1 -Wall -Wextra -o main
+//build gcc main.c -std=c99 -I/usr/include/dbus-1.0 -I/usr/lib/arm-linux-gnueabihf/dbus-1.0/include/ -I/usr/include/glib-2.0 -I/usr/lib/arm-linux-gnueabihf/glib-2.0/include/ -ldbus-1 -ldbus-glib-1 -Wall -Wextra -o bin/main
 
 //Writes 3 bytes of data over and over
 void writeDisk(char data[])
@@ -124,19 +125,36 @@ void decodeDiskInfo()
     }
 }
 
-void testDBus()
+static GMainLoop *loop;
+
+static void
+device_removed (DBusGProxy *proxy,
+         char *ObjectPath[],
+         char *word_eol[],
+         guint hook_id,
+         guint context_id,
+         gpointer user_data)
+{
+    printf("device changed: %s\n", (char*)word_eol);
+    printf("Removed Device\nObjectPath: %s\nhook_id: %d\ncontext_id: %d\nuser_data: %d\n\n", (char*)ObjectPath, hook_id, context_id, (int)user_data); 
+}
+
+void testUDisks()
 {
 DBusGConnection *connection;
+  DBusMessage* message;
   GError *error;
   DBusGProxy *proxy;
-  char **name_list;
-  char **name_list_ptr;
-  
+  gchar *m1;
+  gchar *m2;
+  char *object_path;
+  GPtrArray* ret;
+
   g_type_init ();
 
   error = NULL;
-  connection = dbus_g_bus_get (DBUS_BUS_SESSION,
-                               &error);
+  connection = dbus_g_bus_get(DBUS_BUS_SYSTEM,NULL);
+
   if (connection == NULL)
     {
       g_printerr ("Failed to open connection to bus: %s\n",
@@ -144,50 +162,60 @@ DBusGConnection *connection;
       g_error_free (error);
       exit (1);
     }
+    else
+    {
+        printf("Got a connection to DBUS_BUS_SYSTEM\n");
+    }
+
 
   /* Create a proxy object for the "bus driver" (name "org.freedesktop.DBus") */
-  
-  proxy = dbus_g_proxy_new_for_name (connection,
-                                     DBUS_SERVICE_DBUS,
-                                     DBUS_PATH_DBUS,
-                                     DBUS_INTERFACE_DBUS);
 
-  /* Call ListNames method, wait for reply */
-  error = NULL;
-  if (!dbus_g_proxy_call (proxy, "ListNames", &error, G_TYPE_INVALID,
-                          G_TYPE_STRV, &name_list, G_TYPE_INVALID))
-    {
-      /* Just do demonstrate remote exceptions versus regular GError */
-      if (error->domain == DBUS_GERROR && error->code == DBUS_GERROR_REMOTE_EXCEPTION)
-        g_printerr ("Caught remote method exception %s: %s",
-	            dbus_g_error_get_name (error),
-	            error->message);
-      else
-        g_printerr ("Error: %s\n", error->message);
-      g_error_free (error);
-      exit (1);
-    }
+     proxy=dbus_g_proxy_new_for_name(connection,"org.freedesktop.UDisks","/org/freedesktop/UDisks","org.freedesktop.UDisks");
 
-  /* Print the results */
- 
-  g_print ("Names on the message bus:\n");
-  
-  for (name_list_ptr = name_list; *name_list_ptr; name_list_ptr++)
-    {
-      g_print ("  %s\n", *name_list_ptr);
-    }
-  g_strfreev (name_list);
+     if(proxy == NULL)
+     {
+        g_printerr ("Failed To Create A proxy...: %s\n", error->message);
+        g_error_free (error);
+        exit(1);
+     }
+     else
+       printf("Probably got a connection to the correct interface...\n");
+//It works for me without marshaler register, add and connect to the signals directly
+     m1=g_cclosure_marshal_VOID__STRING;
+     m2=g_cclosure_marshal_VOID__STRING;
 
-  g_object_unref (proxy);
+     dbus_g_object_register_marshaller(m1,G_TYPE_NONE,G_TYPE_STRING,G_TYPE_INVALID);
+
+     dbus_g_object_register_marshaller(m2,G_TYPE_NONE,G_TYPE_STRING,G_TYPE_INVALID);
+
+    
+
+     dbus_g_proxy_add_signal(proxy,"DeviceChanged",DBUS_TYPE_G_OBJECT_PATH, G_TYPE_INVALID);
+     dbus_g_proxy_connect_signal(proxy,"DeviceChanged",(GCallback)device_removed,NULL,NULL);
+
+     loop=g_main_loop_new(NULL,FALSE);
+
+     g_main_loop_run (loop);
+
+     g_error_free (error);
 }
 
 int main(int argc, const char * argv[])
 {
-    //createVarietyDisk(3, ESPRESSO);
-    //createTimeDisk(12, 34);
+    if(argc <= 1) {
+        printf("Not enough arguments. Use --monitor-disks or --make-coffee\n");
+        return;
+    }
+
+    if(strcmp(argv[1], "--monitor-disks")) {
+        printf("yeah pifef\n");
+    }
+    createVarietyDisk(3, ESPRESSO);
+    createTimeDisk(12, 34);
     
     //decodeDiskInfo();
-testDBus();
+//testDBus();
+//testUDisks();
 for(int i = 0; i < 5; ++i) 
 {
 	sleep(1);
